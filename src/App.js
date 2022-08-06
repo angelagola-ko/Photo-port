06eaf02daee6911178f4a85d93d16e920f6a742e
Angela Gola <angelagola.ko@gmail.com>
2022-08-06 19:26:58 -0400

    images compiling by category

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other'},
+    { name: 'portraits', description: 'Portraits of ppl in my life'},
+    {name: 'food', description:'Delicious delicacies'},
+    {name: 'landscape', description: 'Fields, farmhouses, waterfalls'},
+  ]);
+
+  const [currentCategory, setCurrentCategory]=useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
